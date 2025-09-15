0b8f145fe423cacfd5188c2165c73624ba1a9336
rahim <154564602+rx7iiim@users.noreply.github.com>
2025-09-15 15:30:48 +0100

    working on oauth

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,42 +7,43 @@ import * as Joi from 'joi';
 import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 import { UserModule } from './user/user.module';
 
-
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
       load: [appConfig],
-       validationSchema: Joi.object({
-    PORT: Joi.number().default(3000),
-    NODE_ENV: Joi.string().valid('development', 'production').default('development'),
-    DB_HOST :Joi.string().default('localhost'),
-    DB_PORT :Joi.number().default(5432),
-    DB_NAME:Joi.string(),
-  }),
+      validationSchema: Joi.object({
+        PORT: Joi.number().default(3000),
+        NODE_ENV: Joi.string()
+          .valid('development', 'production')
+          .default('development'),
+        DB_HOST: Joi.string().default('localhost'),
+        DB_PORT: Joi.number().default(5432),
+        DB_NAME: Joi.string(),
+      }),
     }),
 
-
     TypeOrmModule.forRootAsync({
-    imports: [ConfigModule],
-    inject: [ConfigService],
-    useFactory: (configService: ConfigService): TypeOrmModuleOptions => {
-      return {
-        type: 'postgres',
-        host: configService.get<string>('DB_HOST'),
-        port: parseInt(configService.get<string>('DB_PORT', { infer: true } )|| '5432', 10),
-        username: configService.get<string>('DB_USERNAME'),
-        password: configService.get<string>('DB_PASSWORD'),
-        database: configService.get<string>('DB_NAME'),
-        autoLoadEntities: true,
-        synchronize: true,
-      };
-    },
-  }),
-
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService): TypeOrmModuleOptions => {
+        return {
+          type: 'postgres',
+          host: configService.get<string>('DB_HOST'),
+          port: parseInt(
+            configService.get<string>('DB_PORT', { infer: true }) || '5432',
+            10,
+          ),
+          username: configService.get<string>('DB_USERNAME'),
+          password: configService.get<string>('DB_PASSWORD'),
+          database: configService.get<string>('DB_NAME'),
+          autoLoadEntities: true,
+          synchronize: true,
+        };
+      },
+    }),
 
     UserModule,
-
   ],
   controllers: [AppController],
   providers: [AppService],
