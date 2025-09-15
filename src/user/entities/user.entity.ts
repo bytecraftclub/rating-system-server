acfc8268330e1cf42446f19b925653afd0f389bc
rahim <154564602+rx7iiim@users.noreply.github.com>
2025-09-15 15:34:03 +0100

    working on oauth

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,31 @@
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column({ unique: true })
+  googleId: string;
+
+  @Column({ unique: true })
+  email: string;
+
+  @Column()
+  name: string;
+
+  @Column({ nullable: true })
+  picture?: string;
+
+  @Column({ default: true })
+  isActive: boolean;
+
+  @Column({ default: 0 })
+  score: number;
+}
