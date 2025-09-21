0d29257174ac51d0d1ff0862f3ce2039c2c20730
rahim <154564602+rx7iiim@users.noreply.github.com>
2025-09-21 05:36:25 +0100

    i have finished the back it will be tested tomorrow

--- src/file-upload/file-upload.module.ts
+++ src/file-upload/file-upload.module.ts
@@ -3,9 +3,17 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { FileUploadController } from './file-upload.controller';
 import { FileUploadService } from './file-upload.service';
 import { FileUpload } from './entities/file-upload.entity';
+import { UserModule } from 'src/user/user.module';
+import { User } from 'src/user/entities/user.entity';
+import { task } from './entities/task.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([FileUpload])],
+  imports: [
+    TypeOrmModule.forFeature([FileUpload]),
+    TypeOrmModule.forFeature([User]),
+    TypeOrmModule.forFeature([task]),
+    UserModule,
+  ],
   controllers: [FileUploadController],
   providers: [FileUploadService],
   exports: [FileUploadService],
