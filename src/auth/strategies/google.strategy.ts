0b23dcab8f250d2e0458d6a3f8c9a8e5eb7d07fa
rahim <154564602+rx7iiim@users.noreply.github.com>
2025-10-02 02:28:47 +0100

    fixed types

--- src/auth/strategies/google.strategy.ts
+++ src/auth/strategies/google.strategy.ts
@@ -2,8 +2,6 @@ import { Injectable, Logger } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { Strategy, VerifyCallback } from 'passport-google-oauth20';
 import { AuthService } from '../auth.service';
-import * as dotenv from 'dotenv';
-dotenv.config();
 
 @Injectable()
 export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
